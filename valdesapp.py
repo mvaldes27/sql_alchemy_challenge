a5abb519e8c032c3136e3a12b62b92e5709c1bc2
Maria Valdes <maria.v.valdes@gmail.com>
2021-03-01 21:10:18 -0500

    added python script

---
+++ valdesapp.py
@@ -0,0 +1,22 @@
+from flask import Flask
+#Flask setup
+app = Flask(__name__)
+
+
+# Flask Routes
+@app.route("/")
+def welcome():
+    return (
+        f"Welcome to the Climate API!<br/>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation"
+        f"/api/v1.0/stations"
+        f"/api/v1.0/tobs"
+        f"/api/v1.0/<start>"
+        f"/api/v1.0/<start>/<end>"
+    )
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
